318f44153233b1ea6e2a4f98f5597ace4273b517
Wilbur Naike <nike.codeux@gmail.com>
2018-05-04 01:34:17 -0500

    Update grises.py

--- grises.py
+++ grises.py
@@ -1,6 +1,6 @@
 import cv2
 
-fuente = 	cv2.imread('puppy.jpg')
+fuente = cv2.imread('puppy.jpg')
 copia = cv2.cvtColor(fuente, cv2.COLOR_BGR2GRAY)
 
 #cv2.imwrite('./pics/gris-cs50.jpg', img_grises)
