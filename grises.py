999a83e61cee26666841349287473514714e24ea
Wilbur Naike <nike.codeux@gmail.com>
2018-05-04 01:34:03 -0500

    Update grises.py

--- grises.py
+++ grises.py
@@ -1,12 +1,12 @@
 import cv2
 
-img_original = 	cv2.imread('./pics/test-cs50.jpg')
-img_grises = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
-img_grises = cv2.resize(img_grises, (0,0), fx=0.25, fy=0.25)
+fuente = 	cv2.imread('puppy.jpg')
+copia = cv2.cvtColor(fuente, cv2.COLOR_BGR2GRAY)
 
 #cv2.imwrite('./pics/gris-cs50.jpg', img_grises)
 
-cv2.imshow('original', img_original)
-cv2.imshow('grises', img_grises)
+cv2.imshow('original', fuente)
+cv2.imshow('grises', copia)
 
 cv2.waitKey(0)
+cv2.destroyAllWindows()
